00ef32907c9e80c2c6227be469f66baf82298cf5
Thomas <twatson52@mac.com>
2013-06-13 17:14:06 -0500

    make controller show up in creative tab

--- block/BlockController.java
+++ block/BlockController.java
@@ -4,11 +4,14 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import tpw_rules.connectedmachines.common.ConnectedMachines;
 
 public class BlockController extends Block {
     public BlockController(int id) {
         super(id, Material.iron);
         setUnlocalizedName("Machine Controller");
+        setCreativeTab(ConnectedMachines.tabMachines);
+
         GameRegistry.registerBlock(this, "CMController");
         LanguageRegistry.addName(this, "Machine Controller");
     }
