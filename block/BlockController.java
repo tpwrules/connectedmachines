067c36f9c7ef80595ddaa945fd967793422df27a
Thomas <twatson52@mac.com>
2013-06-13 17:17:16 -0500

    add icon for controller

--- block/BlockController.java
+++ block/BlockController.java
@@ -4,6 +4,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import tpw_rules.connectedmachines.common.ConnectedMachines;
 
 public class BlockController extends Block {
@@ -14,5 +15,10 @@ public class BlockController extends Block {
 
         GameRegistry.registerBlock(this, "CMController");
         LanguageRegistry.addName(this, "Machine Controller");
+    }
+
+    @Override
+    public void registerIcons(IconRegister iconRegister) {
+        this.blockIcon = iconRegister.registerIcon("connectedmachines:blockController");
     }
 }
