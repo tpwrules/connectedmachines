e918524869102b51556d1daa3d3c0711b36aa4b7
Thomas <twatson52@mac.com>
2013-06-13 11:48:55 -0500

    Created base classes

---
+++ ClientProxy.java
@@ -0,0 +1,11 @@
+package tpw_rules.connectedmachines;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thomaswatson
+ * Date: 6/13/13
+ * Time: 11:47 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ClientProxy {
+}
