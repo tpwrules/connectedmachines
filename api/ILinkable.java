2db95572c67a44ef73c19654e2c1fd0e8f7a4626
Thomas <twatson52@mac.com>
2013-06-13 19:05:45 -0500

    make controller linkable

--- api/ILinkable.java
+++ api/ILinkable.java
@@ -4,6 +4,5 @@ package tpw_rules.connectedmachines.api;
 import tpw_rules.connectedmachines.tile.TileController;
 
 public interface ILinkable {
-    public boolean connected = false;
-    public TileController tileController = null;
+    public void setLink(TileController newController);
 }
--- tile/TileController.java
+++ tile/TileController.java
@@ -2,7 +2,14 @@ package tpw_rules.connectedmachines.tile;
 
 
 import net.minecraft.tileentity.TileEntity;
+import tpw_rules.connectedmachines.api.ILinkable;
 
-public class TileController extends TileEntity {
+public class TileController extends TileEntity implements ILinkable {
+    public boolean linkConnected = false;
+    public TileController tileController = null;
 
+    public void setLink(TileController newController) {
+        tileController = newController;
+        linkConnected = !(newController == null);
+    }
 }
