72dfd05ebaef2992cce70a69e7f5782acf596af5
Thomas <twatson52@mac.com>
2013-06-14 17:04:38 -0500

    add server and client logger

--- util/Util.java
+++ util/Util.java
@@ -1,5 +1,18 @@
 package tpw_rules.connectedmachines.util;
 
 
+import cpw.mods.fml.client.FMLClientHandler;
+
 public class Util {
+    public boolean debugging = true;
+
+    public void log(String msg) {
+        if (!debugging) return;
+        if (FMLClientHandler.instance().getClient().theWorld.isRemote) {
+            System.out.print("[SERVER] ");
+        } else {
+            System.out.print("[CLIENT] ");
+        }
+        System.out.println(msg);
+    }
 }
