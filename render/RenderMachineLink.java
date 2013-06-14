f8fa7b378da7461966fda38fd0796cc96c36f165
Thomas <twatson52@mac.com>
2013-06-14 12:55:23 -0500

    machine link now renders

--- render/RenderMachineLink.java
+++ render/RenderMachineLink.java
@@ -5,6 +5,9 @@ import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.common.ForgeDirection;
+import tpw_rules.connectedmachines.tile.TileMachineLink;
+
 
 public class RenderMachineLink implements ISimpleBlockRenderingHandler {
     public int renderID;
@@ -19,7 +22,39 @@ public class RenderMachineLink implements ISimpleBlockRenderingHandler {
 
     @Override
     public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer) {
-        return false;
+        // get tile for this machine link
+        TileMachineLink tile = (TileMachineLink)world.getBlockTileEntity(x, y, z);
+
+        // render center piece
+        renderer.setRenderBounds(.4, .4, .4, .6, .6, .6);
+        renderer.renderStandardBlock(block, x, y, z);
+
+        // render connecting pieces
+        if (tile.connectedNeighbors[ForgeDirection.EAST.ordinal()]) {
+            renderer.setRenderBounds(.6, .4, .4, 1, .6, .6);
+            renderer.renderStandardBlock(block, x, y, z);
+        }
+        if (tile.connectedNeighbors[ForgeDirection.WEST.ordinal()]) {
+            renderer.setRenderBounds(0, .4, .4, .4, .6, .6);
+            renderer.renderStandardBlock(block, x, y, z);
+        }
+        if (tile.connectedNeighbors[ForgeDirection.UP.ordinal()]) {
+            renderer.setRenderBounds(.4, .6, .4, .6, 1, .6);
+            renderer.renderStandardBlock(block, x, y, z);
+        }
+        if (tile.connectedNeighbors[ForgeDirection.DOWN.ordinal()]) {
+            renderer.setRenderBounds(.4, 0, .4, .6, .4, .6);
+            renderer.renderStandardBlock(block, x, y, z);
+        }
+        if (tile.connectedNeighbors[ForgeDirection.SOUTH.ordinal()]) {
+            renderer.setRenderBounds(.4, .4, .6, .6, .6, 1);
+            renderer.renderStandardBlock(block, x, y, z);
+        }
+        if (tile.connectedNeighbors[ForgeDirection.NORTH.ordinal()]) {
+            renderer.setRenderBounds(.4, .4, 0, .6, .6, .4);
+            renderer.renderStandardBlock(block, x, y, z);
+        }
+        return true;
     }
 
     @Override
