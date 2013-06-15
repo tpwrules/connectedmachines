9e137f45d5122852438d5ef74cae1d1308624d67
Thomas <twatson52@mac.com>
2013-06-14 20:45:55 -0500

    explanation

--- network/PacketType.java
+++ network/PacketType.java
@@ -4,5 +4,6 @@ package tpw_rules.connectedmachines.network;
 public enum PacketType {
     MACHINE_LINK_REDRAW;
 
+    // store int -> enum map because java is drain-bamaged and can't do it automatically
     public static PacketType[] values = PacketType.values();
 }
