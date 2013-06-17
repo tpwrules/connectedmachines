a77197ff82dc2500471077f2133e86946619f16d
Thomas <twatson52@mac.com>
2013-06-17 15:59:29 -0500

    exorcise TileConnected

--- tile/TileController.java
+++ tile/TileController.java
@@ -1,8 +1,42 @@
 package tpw_rules.connectedmachines.tile;
 
 
-public class TileController extends TileConnected {
-    public TileController() {
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.INetworkManager;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.Packet132TileEntityData;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.ForgeDirection;
 
+public class TileController extends TileEntity {
+    public ForgeDirection facing;
+
+    public TileController() {
+        facing = ForgeDirection.UP;
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound tag) {
+        super.readFromNBT(tag);
+        facing = ForgeDirection.getOrientation(tag.getByte("facing"));
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound tag) {
+        super.writeToNBT(tag);
+        tag.setByte("facing", (byte)facing.ordinal());
+    }
+
+    @Override
+    public void onDataPacket(INetworkManager net, Packet132TileEntityData packet) {
+        this.readFromNBT(packet.customParam1);
+    }
+
+    @Override
+    public Packet getDescriptionPacket()
+    {
+        NBTTagCompound tag = new NBTTagCompound();
+        this.writeToNBT(tag);
+        return new Packet132TileEntityData(this.xCoord, this.yCoord, this.zCoord, 0, tag);
     }
 }
