2b9e5f7a1451f8eb3cfd91e262161398164cacac
Thomas <twatson52@mac.com>
2013-06-20 11:13:51 -0500

    add reverse reference

--- gui/GuiType.java
+++ gui/GuiType.java
@@ -2,5 +2,8 @@ package tpw_rules.connectedmachines.gui;
 
 
 public enum GuiType {
-    GUI_CONNECTED_GENERATOR
+    GUI_CONNECTED_GENERATOR;
+
+    // store int -> enum map because java is drain-bamaged and can't do it automatically
+    public static GuiType[] values = GuiType.values();
 }
