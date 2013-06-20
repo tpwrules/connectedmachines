10ca0805d737c4f74c630b1cd07190a9dbca3983
Thomas <twatson52@mac.com>
2013-06-20 16:44:35 -0500

    add gui to controller

--- gui/GuiController.java
+++ gui/GuiController.java
@@ -1,18 +1,72 @@
 package tpw_rules.connectedmachines.gui;
 
 
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.util.StatCollector;
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.GL11;
 import tpw_rules.connectedmachines.tile.TileController;
 
 public class GuiController extends GuiContainer {
     private TileController controller;
 
+    private GuiTextField groupName;
+    private GuiTextField inputName;
+    private GuiTextField outputName;
+
     public GuiController(TileController controller) {
         super(new ContainerController(controller));
         this.controller = controller;
-        ySize = 89;
+        ySize = 98;
+    }
+
+    @Override
+    public void updateScreen() {
+        groupName.updateCursorCounter();
+        super.updateScreen();
+    }
+
+    @Override
+    public void initGui() {
+        super.initGui();
+        Keyboard.enableRepeatEvents(true);
+        buttonList.clear();
+        buttonList.add(new GuiButton(0, 20+guiLeft, 26+guiTop, 10, 20, "<"));
+        buttonList.add(new GuiButton(1, 154+guiLeft, 26+guiTop, 10, 20, ">"));
+        buttonList.add(new GuiButton(2, 20+guiLeft, 48+guiTop, 10, 20, "<"));
+        buttonList.add(new GuiButton(3, 154+guiLeft, 48+guiTop, 10, 20, ">"));
+        buttonList.add(new GuiButton(4, 20+guiLeft, 70+guiTop, 10, 20, "<"));
+        buttonList.add(new GuiButton(5, 154+guiLeft, 70+guiTop, 10, 20, ">"));
+        buttonList.add(new GuiButton(6, 90+guiLeft, 4+guiTop, 40, 20, "Set"));
+        buttonList.add(new GuiButton(7, 132+guiLeft, 4+guiTop, 40, 20, "New"));
+        groupName = new GuiTextField(this.fontRenderer, 32, 26, 120, 20);
+        inputName = new GuiTextField(this.fontRenderer, 32, 48, 120, 20);
+        outputName = new GuiTextField(this.fontRenderer, 32, 70, 120, 20);
+    }
+
+    @Override
+    public void onGuiClosed() {
+        Keyboard.enableRepeatEvents(false);
+    }
+
+    @Override
+    protected void actionPerformed(GuiButton button) {
+
+    }
+
+    @Override
+    protected void keyTyped(char par1, int par2) {
+        groupName.textboxKeyTyped(par1, par2);
+        if (par2 == 1) // escape
+            super.keyTyped(par1, par2);
+    }
+
+    @Override
+    protected void mouseClicked(int x, int y, int button) {
+        super.mouseClicked(x, y, button);
+        groupName.mouseClicked(x-guiLeft, y-guiTop, button);
     }
 
     @Override
@@ -20,6 +74,13 @@ public class GuiController extends GuiContainer {
         fontRenderer.drawString("Controller", 8, 6, 0x404040);
         fontRenderer.drawString("Power: "+controller.powerBuffer+"/"+controller.powerBufferMax,
                 16, 16, 0x404040);
+        fontRenderer.drawString("G:", 8, 32, 0x404040);
+        fontRenderer.drawString("I:", 8, 54, 0x404040);
+        fontRenderer.drawString("O:", 8, 76, 0x404040);
+        super.drawGuiContainerForegroundLayer(par1, par2);
+        groupName.drawTextBox();
+        inputName.drawTextBox();
+        outputName.drawTextBox();
     }
 
     @Override
