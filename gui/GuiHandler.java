9b69e53acf9c27a0e23ea61c60ae6fc0ce0b6572
Thomas <twatson52@mac.com>
2013-06-20 11:15:28 -0500

    add gui to handler

--- gui/GuiHandler.java
+++ gui/GuiHandler.java
@@ -3,18 +3,30 @@ package tpw_rules.connectedmachines.gui;
 
 import cpw.mods.fml.common.network.IGuiHandler;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
+import tpw_rules.connectedmachines.tile.TileConnectedGenerator;
 
 public class GuiHandler implements IGuiHandler {
     @Override
     public Object getServerGuiElement(int id, EntityPlayer player, World world,
                                       int x, int y, int z) {
+        TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
+        switch (GuiType.values[id]) {
+            case GUI_CONNECTED_GENERATOR:
+                return new ContainerConnectedGenerator(player.inventory, (TileConnectedGenerator)tileEntity);
+        }
         return null;
     }
 
     @Override
     public Object getClientGuiElement(int id, EntityPlayer player, World world,
                                       int x, int y, int z) {
+        TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
+        switch (GuiType.values[id]) {
+            case GUI_CONNECTED_GENERATOR:
+                return new GuiConnectedGenerator(player.inventory, (TileConnectedGenerator)tileEntity);
+        }
         return null;
     }
 }
